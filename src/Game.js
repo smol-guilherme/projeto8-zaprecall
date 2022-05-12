735fa3c8b96ad7c75a8ae8f5eb512109a54f89bd
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-05-11 23:47:30 -0300

    As cartas agora são reveladas corretamente. As cartas agora viram de pergunta para resposta individualmente. A contagem de cards respondidos agora é computada corretamente.

--- src/Game.js
+++ src/Game.js
@@ -10,119 +10,127 @@ function Cover({ reveal }) {
     )
 }
 
-function Cards({ deck, count }) {
+function Cards({ index, card, flipCard, setResult }) {
     return(
-        <> 
+        <>
             {
-                deck.map((item, index) => !item.flipped ? 
-                <Question key={index} question={item.question} flipAction={() => flipCard(index)}/> : 
-                <Answer count={count} key={index} answer={item.answer} deck={deck} />)
+                !card.flipped 
+                ? <Question key={index} flipCard={flipCard} index={index} question={card.question} />
+                : <Answer key={index} card={card} answer={card.answer} setResult={setResult} index={index} />
             }
         </>
     )
 }
 
-function Question({ question, flipAction }) {
+function Question({ index, question, flipCard }) {
     return (
         <div className="card">
             <div className="text-content">
                 {question}
-            </div> 
-            { <Flip flip={flipAction} /> }
+            </div>
+            <div className="flip">
+                <img onClick={() => flipCard(index)} src="./content/flip.png" />
+            </div>
         </div>
     )
 }
 
-function Answer({ deck, answer, index, count }) {
-    function updateData(index, result) {
-        count()
-        deck[index].state.answer
-    }
-    
+function Answer({ index, answer, setResult }) {
     return (
         <div className="card">
             <div className="text-content">
                 {answer}
             </div>
             <div className="button-box">
-                <div onClick={() => updateData(index, 1)} className="state-1">Não lembrei</div>
-                <div onClick={() => updateData(index, 2)} className="state-2">Quase não lembrei</div>
-                <div onClick={() => updateData(index, 3)} className="state-3">Zap!</div>
+                <div onClick={() => setResult(index, 1)} className="state-1">Não lembrei</div>
+                <div onClick={() => setResult(index, 2)} className="state-2">Quase não lembrei</div>
+                <div onClick={() => setResult(index, 3)} className="state-3">Zap!</div>
             </div>
         </div>
     )
 }
 
-function Flip({ flip }) {
-    return (
-        <div className="flip">
-            <img onClick={flip} src="./content/flip.png" />
-        </div>
-    )
-}
+// <ion-icon name="help-circle"></ion-icon>
+// <ion-icon name="checkmark-circle"></ion-icon>
+// <ion-icon name="close-circle"></ion-icon>
 
-function Bottom({ count }) {
+function Bottom({ count, answers }) {
+    const icons = ["close-circle", "help-circle", "checkmark-circle"]
     return (
         <div className="progress">
-            {count}/4 CONCLUÍDOS
+            { count }/4 CONCLUÍDOS
+            { answers.length === 0 ? " " : answers.map((icon) => <ion-icon name={icons[icon-1]}></ion-icon>) }
         </div>
     )
 }
 
 export default function Game({ start }) {
-    const done = { answer: 0, selectable: true };
+    const [count, setCount] = React.useState(0);
+    const [answers, setAnswers] = React.useState([])
     const [cards, setCards] = React.useState([
         {   
             question: 'Pergunta 01?',
             answer: 'Resposta 01!',
             theme: 'default',
             flipped: false,
-            state: done
+            revealed: false,
+            result: undefined
         },
         {
             question: 'Pergunta 02?',
             answer: 'Resposta 02!',
             theme: 'default',
             flipped: false,
-            state: done
+            revealed: false,
+            result: undefined
         },
         {
             question: 'Pergunta 03?',
             answer: 'Resposta 03!',
             theme: 'default',
             flipped: false,
-            state: done
+            revealed: false,
+            result: undefined
         },
         {
             question: 'Pergunta 04?',
             answer: 'Resposta 04!',
             theme: 'default',
             flipped: false,
-            state: done
+            revealed: false,
+            result: undefined
         }
     ]);
-    
-    function flipCard(index) {
-        cards[index].flipped = !cards[index].flipped;
-        setCards([...cards])
-    }
-    
-    const [count, setCount] = React.useState(0)
-    const [revealed, setRevealed] = React.useState(false)
 
-    function countAnswer() {
-        setCount(count + 1)
+    function revealCard(revealIndex) {
+        cards[revealIndex].revealed = true;
+        setCards([ ...cards ]);
     }
 
-    function revealCard() {
-        setRevealed(!revealed)
+    function flipCard(flipIndex) {
+        cards[flipIndex].flipped = true
+        setCards([ ...cards ]);
+    }
+
+    function setResult(resultIndex, result = 0) {
+        if (cards[resultIndex].result === undefined) {
+            cards[resultIndex].result = result;
+            setCount(count + 1);
+            setAnswers(answers + result)
+            console.log(answers)
+            setCards([ ...cards ]);
+        }
     }
 
     return (
         <div className="main-screen">
             <Logo />
-            { !revealed ? <Cover reveal={revealCard} /> : <Cards done={setDone} count={countAnswer} deck={cards} /> }
-            <Bottom count={count} />
+            { 
+                cards.map((card, index) => !card.revealed 
+                ? <Cover reveal={() => revealCard(index)} /> 
+                : <Cards key={index} setResult={setResult} flipCard={flipCard} index={index} card={card} /> )
+            }
+            <Bottom results={answers} count={count} />
         </div>
     )
 }
